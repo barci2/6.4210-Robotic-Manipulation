5bfad0c88b3fe404d1280033c019820f4d2e298e
Bartłomiej Cieślar <bcieslar2001@gmail.com>
2023-11-08 22:34:39 -0500

    Initial Setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# 6.4210-Robotic-Manipulation
+# Robotic Manipulation project
+## Running
+```
+python3 main.py
+```
+
+## TODO
+ - Add cameras
+ - Add flying object
+ - Add skeleton for perception and grasping
+ - Add hand
---
+++ main.py
@@ -0,0 +1,38 @@
+from pydrake.all import DiagramBuilder, Diagram
+from pydrake.geometry import StartMeshcat
+from pydrake.systems.analysis import Simulator
+from manipulation.station import MakeHardwareStation, load_scenario
+from manipulation.scenarios import AddRgbdSensors
+import matplotlib.pyplot as plt
+from utils import diagram_update_meshcat, station_visualize_camera, diagram_visualize_connections
+
+# Settings
+scenario_path = "scenario.yaml"
+close_button_str = "close"
+
+# Setting up meshcat
+meshcat = StartMeshcat()
+meshcat.AddButton(close_button_str)
+
+# Setting up the main diagram
+builder = DiagramBuilder()
+
+scenario = load_scenario(filename=scenario_path)
+station = builder.AddSystem(MakeHardwareStation(scenario, meshcat=meshcat))
+
+diagram = builder.Build()
+diagram.set_name("object_catching_system")
+
+# Visualization
+diagram_update_meshcat(diagram)
+diagram_visualize_connections(diagram, "diagram.svg")
+station_visualize_camera(station, "camera0")
+
+# Setting up the simulation
+simulator = Simulator(diagram)
+context = diagram.CreateDefaultContext()
+
+while not meshcat.GetButtonClicks(close_button_str):
+    pass
+
+
