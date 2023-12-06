4f62aebb48a4b4f08194785f988d7e42a0aef39f
Bartłomiej Cieślar <bcieslar2001@gmail.com>
2023-12-06 00:52:00 -0500

    point cloud generation for the thrown object
    Signed-off-by: Bartłomiej Cieślar <bcieslar2001@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,28 +1,21 @@
 from pydrake.all import (
     DiagramBuilder,
     StartMeshcat,
-    MeshcatVisualizer,
-    MeshcatVisualizerParams,
     Simulator,
     ModelInstanceIndex,
     PiecewisePolynomial,
-    TrajectorySource
+    TrajectorySource,
+    RigidTransform
 )
-# from pydrake.systems.analysis import Simulator
-from manipulation.meshcat_utils import WsgButton
 from manipulation.station import MakeHardwareStation, load_scenario
-from manipulation.scenarios import AddRgbdSensors, AddShape
-from manipulation.meshcat_utils import AddMeshcatTriad, PublishPositionTrajectory
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import time
 from utils import (
-    visualize_camera_plt,
     diagram_visualize_connections,
     throw_object,
 )
-from perception import TrajectoryPredictor, add_cameras
+from perception import PointCloudGenerator, TrajectoryPredictor, add_cameras
 
 from grasping_selection import GraspSelector
 from motion_tests import motion_test  # TEMPORARY
@@ -33,19 +26,25 @@ close_button_str = "Close"
 scenario_file = "data/scenario.yaml"
 thrown_obj_prefix = "obj"
 this_drake_module_name = "cwd"
+point_cloud_cameras_center = [0, 0, 100]
 simulator_runtime = 3
 
 np.random.seed(seed)
 
-##### Meshcat Setup #####
+#####################
+### Meshcat Setup ###
+#####################
 meshcat = StartMeshcat()
 meshcat.AddButton(close_button_str)
 
-##### Diagram Setup #####
+#####################
+### Diagram Setup ###
+#####################
 builder = DiagramBuilder()
 scenario = load_scenario(filename=scenario_file)
 
-##### Hardware station setup #####
+
+### Hardware station setup
 station = builder.AddSystem(MakeHardwareStation(
     scenario=scenario,
     meshcat=meshcat,
@@ -56,7 +55,7 @@ station = builder.AddSystem(MakeHardwareStation(
 ))
 scene_graph = station.GetSubsystemByName("scene_graph")
 
-### Object to throw
+### Extracting the object to throw
 plant = station.GetSubsystemByName("plant")
 (obj_name,) = [ # There should only be one uncommented object
     model_name
@@ -66,18 +65,55 @@ plant = station.GetSubsystemByName("plant")
 ]
 
 ### Camera Setup
-cameras, camera_transforms = add_cameras(builder, station, plant, 800, 600, 8, 4, 5)
+icp_cameras, icp_camera_transforms = add_cameras(
+    builder=builder,
+    station=station,
+    plant=plant,
+    camera_width=800,
+    camera_height=600,
+    horizontal_num=8,
+    vertical_num=5,
+    camera_distance=5,
+    cameras_center=[0, 0, 0]
+)
+point_cloud_cameras, point_cloud_camera_transforms = add_cameras(
+    builder=builder,
+    station=station,
+    plant=plant,
+    camera_width=800,
+    camera_height=600,
+    horizontal_num=8,
+    vertical_num=4,
+    camera_distance=5,
+    cameras_center=point_cloud_cameras_center
+)
 
-### Perception Setup
-perception_system = builder.AddSystem(TrajectoryPredictor(cameras, camera_transforms, 0, obj_name, plant, meshcat))
-for i, camera in enumerate(cameras):
-    depth_input, label_input = perception_system.camera_input_ports(i)
-    builder.Connect(camera.depth_image_32F_output_port(), depth_input)
-    builder.Connect(camera.label_image_output_port(), label_input)
+### Point Cloud Capturing Setup
+obj_point_cloud_system = builder.AddSystem(PointCloudGenerator(
+    cameras=point_cloud_cameras,
+    camera_transforms=point_cloud_camera_transforms,
+    cameras_center=point_cloud_cameras_center,
+    pred_thresh=5,
+    thrown_model_name=obj_name,
+    plant=plant
+))
+obj_point_cloud_system.ConnectCameras(builder, point_cloud_cameras)
+
+### Trajectory Prediction Setup
+traj_pred_system = builder.AddSystem(TrajectoryPredictor(
+    cameras=icp_cameras,
+    camera_transforms=icp_camera_transforms,
+    pred_thresh=5,
+    thrown_model_name=obj_name,
+    plant=plant,
+    meshcat=meshcat
+))
+traj_pred_system.ConnectCameras(builder, icp_cameras)
+builder.Connect(obj_point_cloud_system.GetOutputPort("point_cloud"), traj_pred_system.point_cloud_input_port)
 
 ### Grasp Selector
 # grasp_selector = builder.AddSystem(GraspSelector(plant, scene_graph, diagram, context, meshcat))
-# builder.Connect(perception_system.GetOutputPort("object_trajectory"), grasp_selector.GetInputPort("object_trajectory"))
+# builder.Connect(traj_pred_system.GetOutputPort("object_trajectory"), grasp_selector.GetInputPort("object_trajectory"))
 
 ### Motion Planning
 # linear path for testing
@@ -101,30 +137,36 @@ context = diagram.CreateDefaultContext()
 diagram.set_name("object_catching_system")
 diagram_visualize_connections(diagram, "diagram.svg")
 
-##### Simulation Setup #####
+########################
+### Simulation Setup ###
+########################
 simulator = Simulator(diagram)
 simulator_context = simulator.get_mutable_context()
 station_context = station.GetMyMutableContextFromRoot(simulator_context)
 plant_context = plant.GetMyMutableContextFromRoot(simulator_context)
 
-# station.GetInputPort("iiwa.position").FixValue(station_context, np.zeros(7))
-station.GetInputPort("wsg.position").FixValue(station_context, [1])
-
+### Capturing the point cloud for the robot
 obj = plant.GetModelInstanceByName(obj_name)
-joint_idx = plant.GetJointIndices(obj)[0]  # JointIndex object
-joint = plant.get_joint(joint_idx)  # Joint object
-joint.Lock(plant_context)
+body_idx = plant.GetBodyIndices(obj)[0]
+body = plant.get_body(body_idx)
+plant.SetFreeBodyPose(plant_context, body, RigidTransform(point_cloud_cameras_center))
+obj_point_cloud_system.CapturePointCloud(obj_point_cloud_system.GetMyMutableContextFromRoot(simulator_context))
+
+
+### Opening the gripper
+# station.GetInputPort("iiwa.position").FixValue(station_context, np.zeros(7)) # TESTING
+station.GetInputPort("wsg.position").FixValue(station_context, [1]) # TESTING
 
 throw_object(plant, plant_context, obj_name)
 
-# scene_graph_context = scene_graph.GetMyMutableContextFromRoot(simulator_context)
-# query_object = scene_graph.get_query_output_port().Eval(scene_graph_context)
-# print(query_object.ComputeSignedDistanceToPoint([-2, 0, 0]))
+# scene_graph_context = scene_graph.GetMyMutableContextFromRoot(simulator_context) # TESTING
+# query_object = scene_graph.get_query_output_port().Eval(scene_graph_context) # TESTING
+# print(query_object.ComputeSignedDistanceToPoint([-2, 0, 0])) # TESTING
 
-##### Running Simulation & Meshcat #####
-# simulator.AdvanceTo(0.2)
-
-simulator.set_target_realtime_rate(1.0)
+####################################
+### Running Simulation & Meshcat ###
+####################################
+simulator.set_target_realtime_rate(0.1)
 simulator.set_publish_every_time_step(True)
 plt.show()
 
--- src/perception.py
+++ src/perception.py
@@ -27,7 +27,8 @@ from pydrake.all import (
     OutputPort,
     Rgba,
     Meshcat,
-    PiecewisePolynomial
+    PiecewisePolynomial,
+    Value
 )
 import numpy as np
 import numpy.typing as npt
@@ -35,6 +36,7 @@ import itertools
 import matplotlib.pyplot as plt
 
 
+group_idx = 0
 def add_cameras(
         builder: DiagramBuilder,
         station: Diagram,
@@ -43,8 +45,10 @@ def add_cameras(
         camera_height: int,
         horizontal_num: int,
         vertical_num: int,
-        camera_distance: float
+        camera_distance: float,
+        cameras_center: npt.NDArray[np.float32]
     ) -> Tuple[List[RgbdSensor], List[RigidTransform]]:
+    global group_idx
     camera_config = CameraConfig()
     camera_config.width = camera_width
     camera_config.height = camera_height
@@ -58,9 +62,9 @@ def add_cameras(
     thetas = np.linspace(0, 2*np.pi, horizontal_num, endpoint=False)
     phis = np.linspace(0, -np.pi, vertical_num + 2)[1:-1]
     for idx, (theta, phi) in enumerate(itertools.product(thetas, phis)):
-        name = f"camera{idx}"
+        name = f"camera{idx}_group{group_idx}"
         transform = RigidTransform(RollPitchYaw(0, 0, theta).ToRotationMatrix(
-        ) @ RollPitchYaw(phi, 0, 0).ToRotationMatrix(), np.zeros(3)) @ RigidTransform([0, 0, -camera_distance])
+        ) @ RollPitchYaw(phi, 0, 0).ToRotationMatrix(), cameras_center) @ RigidTransform([0, 0, -camera_distance])
 
         _, depth_camera = camera_config.MakeCameras()
         camera_sys = builder.AddSystem(RgbdSensor(
@@ -86,25 +90,22 @@ def add_cameras(
         camera_systems.append(camera_sys)
         camera_transforms.append(transform)
 
+    group_idx += 1
     return camera_systems, camera_transforms
 
-
-class TrajectoryPredictor(LeafSystem):
-    """
-    Performs ICP after first keying out the objects
-    """
+class CameraBackedSystem(LeafSystem):
     def __init__(
             self,
             cameras: List[RgbdSensor],
             camera_transforms: List[RigidTransform],
             pred_thresh: int,
-            thrown_model_name: int,
+            thrown_model_name: str,
             plant: MultibodyPlant,
-            meshcat: Optional[Meshcat] = None
+            meshcat: Optional[Meshcat] = None,
         ):
         super().__init__()
+
         self._pred_thresh = pred_thresh
-        self._num_cameras = len(cameras)
         self._camera_infos = [camera.depth_camera_info() for camera in cameras]
         self._camera_transforms = camera_transforms
         self._meshcat = meshcat
@@ -123,11 +124,127 @@ class TrajectoryPredictor(LeafSystem):
             f"camera{i}.label_input", AbstractValue.Make(ImageLabel16I(camera_info.width(), camera_info.height())))
             for i, camera_info in enumerate(self._camera_infos)]
 
+    def camera_input_ports(self, camera_idx: int) -> Tuple[InputPort, InputPort]:
+        return self._camera_depth_inputs[camera_idx], self._camera_label_inputs[camera_idx]
+
+    def ConnectCameras(self, builder: DiagramBuilder, cameras: List[RgbdSensor]):
+        for camera, depth_input, label_input in zip(cameras, self._camera_depth_inputs, self._camera_label_inputs):
+            builder.Connect(camera.depth_image_32F_output_port(), depth_input)
+            builder.Connect(camera.label_image_output_port(), label_input)
+
+    def GetCameraPoints(self, context: Context) -> npt.NDArray[np.float32]:
+        total_point_cloud = np.zeros((3, 0))
+        for camera_info, transform, depth_input, label_input in zip(
+                self._camera_infos,
+                self._camera_transforms,
+                self._camera_depth_inputs,
+                self._camera_label_inputs
+            ):
+            height = camera_info.height()
+            width = camera_info.width()
+            center_x = camera_info.center_x()
+            center_y = camera_info.center_y()
+            focal_x = camera_info.focal_x()
+            focal_y = camera_info.focal_y()
+
+            depth_img = depth_input.Eval(context).data
+            label_img = label_input.Eval(context).data
+            u_coords, v_coords, _ = np.meshgrid(np.arange(width), np.arange(height), [0], copy=False)
+            distances_coords = np.stack([u_coords, v_coords, depth_img], axis=-1)
+            depth_pixel = distances_coords[np.logical_and(label_img == self._obj_idx, np.abs(depth_img) != np.inf)]
+
+            u = depth_pixel[:, 0]
+            v = depth_pixel[:, 1]
+            z = depth_pixel[:, 2]
+
+            x = (u - center_x) * z / focal_x
+            y = (v - center_y) * z / focal_y
+            pC = np.stack([x, y, z])
+
+            if pC.shape[1] >= self._pred_thresh:
+                total_point_cloud = np.concatenate([total_point_cloud, (transform @ pC)], axis=-1)
+        return total_point_cloud
+
+    def PublishMeshcat(self, points: npt.NDArray[np.float32]):
+        cloud = PointCloud(points.shape[1])
+        if points.shape[1] > 0:
+            cloud.mutable_xyzs()[:] = points
+        if self._meshcat is not None:
+            self._meshcat.SetObject("TrajectoryPredictorPointCloud", cloud, point_size=0.01, rgba=Rgba(1, 0.5, 0.5))
+
+class PointCloudGenerator(CameraBackedSystem):
+    def __init__(
+            self,
+            cameras: List[RgbdSensor],
+            camera_transforms: List[RigidTransform],
+            cameras_center: npt.NDArray[np.float32],
+            pred_thresh: int,
+            thrown_model_name: str,
+            plant: MultibodyPlant,
+            meshcat: Optional[Meshcat] = None,
+        ):
+        super().__init__(
+            cameras=cameras,
+            camera_transforms=camera_transforms,
+            pred_thresh=pred_thresh,
+            thrown_model_name=thrown_model_name,
+            plant=plant,
+            meshcat=meshcat
+        )
+
+        self._cameras_center = cameras_center
+        self._point_cloud = PointCloud()
+        self._point_cloud_output = self.DeclareAbstractOutputPort(
+            'point_cloud',
+            lambda: AbstractValue.Make(PointCloud()),
+            self.OutputPointCloud
+        )
+
+    @property
+    def point_cloud_output_port(self) -> OutputPort:
+        return self._point_cloud_output
+
+    def OutputPointCloud(self, context: Context, output: Value) -> None:
+        output.set_value(self._point_cloud)
+
+    def CapturePointCloud(self, context: Context) -> None:
+        points = (self.GetCameraPoints(context).T - self._cameras_center).T
+        self._point_cloud = PointCloud(points.shape[1])
+        self._point_cloud.mutable_xyzs()[:] = points
+        self.PublishMeshcat(points)
+
+class TrajectoryPredictor(CameraBackedSystem):
+    """
+    Performs ICP after first keying out the objects
+    """
+    def __init__(
+            self,
+            cameras: List[RgbdSensor],
+            camera_transforms: List[RigidTransform],
+            pred_thresh: int,
+            thrown_model_name: str,
+            plant: MultibodyPlant,
+            meshcat: Optional[Meshcat] = None
+        ):
+        super().__init__(
+            cameras=cameras,
+            camera_transforms=camera_transforms,
+            pred_thresh=pred_thresh,
+            thrown_model_name=thrown_model_name,
+            plant=plant,
+            meshcat=meshcat
+        )
+
         # Saved previous poses
         self._poses_state_index = self.DeclareAbstractState(AbstractValue.Make([0.0, 0.0, 0.0]))
 
+        self._obj_point_cloud_input = self.DeclareAbstractInputPort(
+            "obj_point_cloud",
+            AbstractValue.Make(PointCloud())
+        )
+
         # Update Event
-        self.DeclarePeriodicPublishEvent(0.1, 0.001, self.PredictTrajectory)
+        self.DeclarePeriodicPublishEvent(0.001, 0.1, self.PredictTrajectory)
 
         # Michael commented out `lambda c, o: None` and added `self.CreateOutput`
         port = self.DeclareAbstractOutputPort(
@@ -137,14 +254,13 @@ class TrajectoryPredictor(LeafSystem):
             self.CreateOutput,
         )
 
-    def camera_input_ports(self, camera_idx: int) -> Tuple[InputPort, InputPort]:
-        return self._camera_depth_inputs[camera_idx], self._camera_label_inputs[camera_idx]
-
-    def point_cloud_output_port(self) -> OutputPort:
-        return self._point_cloud_output
+    @property
+    def point_cloud_input_port(self) -> OutputPort:
+        return self._obj_point_cloud_input
 
     def PredictTrajectory(self, context: Context):
-        points = np.concatenate([self.ExtractPointCloudForCamera(context, camera_idx) for camera_idx in range(self._num_cameras)], axis=1)
+        self._obj_point_cloud_input.Eval(context)
+        points = self.GetCameraPoints(context)
         if self._meshcat is not None:
             self.PublishMeshcat(points, self._meshcat)
 
@@ -154,32 +270,6 @@ class TrajectoryPredictor(LeafSystem):
         if points.shape[1] > 0:
             cloud.mutable_xyzs()[:] = points
         meshcat.SetObject("TrajectoryPredictorPointCloud", cloud, point_size=0.01, rgba=Rgba(1, 0.5, 0.5))
-
-    def ExtractPointCloudForCamera(self, context: Context, camera_idx: int) -> npt.NDArray[np.float32]:
-        camera_info = self._camera_infos[camera_idx]
-        transform = self._camera_transforms[camera_idx]
-
-        height = camera_info.height()
-        width = camera_info.width()
-        center_x = camera_info.center_x()
-        center_y = camera_info.center_y()
-        focal_x = camera_info.focal_x()
-        focal_y = camera_info.focal_y()
-
-        depth_img = self._camera_depth_inputs[camera_idx].Eval(context).data
-        label_img = self._camera_label_inputs[camera_idx].Eval(context).data
-        u_coords, v_coords, _ = np.meshgrid(np.arange(width), np.arange(height), [0], copy=False)
-        distances_coords = np.stack([u_coords, v_coords, depth_img], axis=-1)
-        depth_pixel = distances_coords[np.logical_and(label_img == self._obj_idx, np.abs(depth_img) != np.inf)]
-
-        u = depth_pixel[:, 0]
-        v = depth_pixel[:, 1]
-        z = depth_pixel[:, 2]
-
-        x = (u - center_x) * z / focal_x
-        y = (v - center_y) * z / focal_y
-        pC = np.stack([x, y, z])
-        return (transform @ pC)
 
     # Michael added this function to test connecting the two leafsystems
     def CreateOutput(self, context, output):
